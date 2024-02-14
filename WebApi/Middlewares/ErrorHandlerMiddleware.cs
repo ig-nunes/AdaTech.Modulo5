72fe18d00a7c883cd9fc08d3e5316ec9a57f8ffb
Igor Nunes <inunes.us@gmail.com>
2024-02-14 13:11:04 -0300

    feat: error handler middleware added

---
+++ WebApi/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next) 
+        {  
+            _next = next; 
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync( new {ErrorMessage = $"Ocorreu algum erro, meu amigo. Mais informações: {ex.Message}"} );
+            }
+        }
+    }
+}
