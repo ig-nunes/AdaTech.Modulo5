3587212edb5a7fb6e0e5d3142f68759ef66001b2
Igor Nunes <inunes.us@gmail.com>
2024-02-14 13:11:31 -0300

    feat: autorization middelware added

---
+++ WebApi/Middlewares/AutorizacaoMiddleware.cs
@@ -0,0 +1,60 @@
+﻿namespace WebApi.Middlewares
+{
+    public class AutorizacaoMiddleware
+    {
+        public readonly RequestDelegate _next;
+
+        public AutorizacaoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //try
+            //{
+            //    if (!context.Request.Headers.ContainsKey("UsuarioLogado"))
+            //    {
+            //        context.Response.StatusCode = 401;
+            //        return;
+            //    }
+            //    if (!context.Request.Headers.TryGetValue("UsuarioLogado", out var value))
+            //    {
+            //        if (value == "Admin")
+            //        {
+            //            await _next(context);
+            //        }
+            //        else
+            //        {
+            //            context.Response.StatusCode = 401;
+            //            return;
+            //        }
+            //    }
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+            if (!context.Request.Headers.ContainsKey("UsuarioLogado"))
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+            if (context.Request.Headers.TryGetValue("UsuarioLogado", out var value))
+            {
+                if (value == "Admin")
+                {
+                    await _next(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+            }
+        }
+
+    }
+}
