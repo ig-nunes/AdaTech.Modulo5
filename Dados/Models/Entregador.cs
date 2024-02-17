8610e7c2662336b95009e29b617d9086ed42f9c2
Igor Nunes <inunes.us@gmail.com>
2024-02-16 22:34:34 -0300

    feat: Entregador class added

---
+++ Dados/Models/Entregador.cs
@@ -0,0 +1,9 @@
+﻿namespace Dados.Models
+{
+    public class Entregador
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
