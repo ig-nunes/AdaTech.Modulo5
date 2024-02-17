777d510783032ee8f476e92241c6c42c9ad9a520
Igor Nunes <inunes.us@gmail.com>
2024-02-16 22:37:58 -0300

    feat: request model added

---
+++ WebApi/RequestModels/EntregadorRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApi.Attributes;
+
+namespace WebApi.RequestModels
+{
+    public class EntregadorRequest
+    {
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O nsome é obrigatório")]
+        [StringLength(30, ErrorMessage = "O nome não pode ter mais de 30 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatório")]
+        [DataType(DataType.Date, ErrorMessage = "Formato de data inválido.")]
+        [MaiorDeIdade(ErrorMessage = "Você deve ser maior de 18 anos.")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
