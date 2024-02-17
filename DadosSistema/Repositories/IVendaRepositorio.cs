8b7313946a54ab61e99366abc9fbf1621aa0d7f6
Igor Nunes <inunes.us@gmail.com>
2024-02-17 20:49:34 -0300

    feat: add repositories

---
+++ DadosSistema/Repositories/IVendaRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using DadosSistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosSistema.Repositories
+{
+    public interface IVendaRepositorio
+    {
+        public Venda Add(Venda venda);
+        public List<Venda> Get();
+        public Venda? GetById(int id);
+
+    }
+}
