6557430ceaffe835b5951fbca2c04712c068b7f0
Igor Nunes <inunes.us@gmail.com>
2024-02-16 22:37:00 -0300

    feat: Custom ApiException added

---
+++ Dados/CustomExceptions/ApiException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados.CustomExceptions
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+        public ApiException(string? message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
