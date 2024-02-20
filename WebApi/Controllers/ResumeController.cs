b0866fee93445a7635259e8ce35a970bd4120bf8
Igor Nunes <inunes.us@gmail.com>
2024-02-20 15:23:22 -0300

    feat: atividade curriculos

---
+++ WebApi/Controllers/ResumeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResumeController : ControllerBase
+    {
+        private readonly IContentTypeProvider _contentTypeProvider;
+
+        public ResumeController(IContentTypeProvider contentTypeProvider)
+        {
+            _contentTypeProvider = contentTypeProvider;
+        }
+
+        [HttpGet]
+        public IActionResult GetResume(string nomeArquivo)
+        {
+            var caminhoArquivo = $"wwwroot/{nomeArquivo}";
+
+            if (!System.IO.File.Exists(caminhoArquivo))
+            {
+                return BadRequest(new
+                {
+                    Titulo = "Erro ao fazer o download do arquivo",
+                    Detalhes = $"Arquivo não existe: {nomeArquivo}",
+                    StatusCode = 400,
+                });
+            }
+
+            var fileInBytes = System.IO.File.ReadAllBytes(caminhoArquivo);
+
+            var contentType = "application/octet-stream";
+            if (_contentTypeProvider.TryGetContentType(caminhoArquivo, out var contentTypeProvided))
+            {
+                contentType = contentTypeProvided;
+            }
+
+            return File(fileInBytes, contentType);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostResume(IFormFile file)
+        {
+            var caminhoArquivo = $"wwwroot/{file.FileName}";
+
+            string[] extensoesPermitidas = [".pdf", ".doc", ".docx"];
+
+            var ExtensaoArquivo = Path.GetExtension(caminhoArquivo);
+
+
+            if (ValidateFile(file) != null)
+            {
+                return ValidateFile(file)!;
+            }
+
+            using (var fileStream = System.IO.File.Create(caminhoArquivo))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return Ok(new { FileName = file.FileName });
+        }
+
+        private IActionResult? ValidateFile(IFormFile file)
+        {
+            var caminhoArquivo = $"wwwroot/{file.FileName}";
+
+            string[] extensoesPermitidas = [".pdf", ".doc", ".docx"];
+
+            var ExtensaoArquivo = Path.GetExtension(caminhoArquivo);
+
+            if (file.Length > 5 * 1024 * 1024)
+            {
+                return BadRequest(new
+                {
+                    Titulo = "Erro ao fazer o upload",
+                    Detalhes = "Tamanho excede o limite permitido",
+                    StatusCode = 400,
+                });
+            }
+            if (!extensoesPermitidas.Contains(ExtensaoArquivo))
+            {
+                return BadRequest(new
+                {
+                    Titulo = "Erro ao fazer upload",
+                    Detalhes = "Extensão não permitida",
+                    StatusCode = 400,
+                });
+            }
+
+            return null;
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using WebApi.Models;
@@ -11,133 +12,137 @@ namespace WebApi
         {
             // Com minimal API:
 
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-            const string url = "/imoveis";
+            //var builder = WebApplication.CreateBuilder(args);
+            //var app = builder.Build();
+            //const string url = "/imoveis";
 
-            var imoveis = new List<Imovel>();
+            //var imoveis = new List<Imovel>();
 
-            app.MapGet($"{url}", () =>
-            {
-                return Results.Ok(imoveis);
-            });
+            //app.MapGet($"{url}", () =>
+            //{
+            //    return Results.Ok(imoveis);
+            //});
 
-            app.MapGet($"{url}/{{id}}", (string id) =>
-            {
-                var imovel = imoveis.FirstOrDefault(x => x.Id == id);
-                if (imovel == null)
-                {
-                    return Results.NotFound();
-                }
-                return Results.Ok(imovel);
-            });
+            //app.MapGet($"{url}/{{id}}", (string id) =>
+            //{
+            //    var imovel = imoveis.FirstOrDefault(x => x.Id == id);
+            //    if (imovel == null)
+            //    {
+            //        return Results.NotFound();
+            //    }
+            //    return Results.Ok(imovel);
+            //});
 
-            app.MapPost(url, (CreateImovel imovelRequest) =>
-            {
-                if (imovelRequest == null || imovelRequest.Endereco == null || imovelRequest.Proprietario == null)
-                {
-                    return Results.BadRequest();
-                }
-                var imovel = new Imovel()
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Endereco = imovelRequest.Endereco,
-                    Proprietario = imovelRequest.Proprietario
-                };
-                imoveis.Add(imovel);
-                return Results.Ok(imovel);
-            });
+            //app.MapPost(url, (CreateImovel imovelRequest) =>
+            //{
+            //    if (imovelRequest == null || imovelRequest.Endereco == null || imovelRequest.Proprietario == null)
+            //    {
+            //        return Results.BadRequest();
+            //    }
+            //    var imovel = new Imovel()
+            //    {
+            //        Id = Guid.NewGuid().ToString(),
+            //        Endereco = imovelRequest.Endereco,
+            //        Proprietario = imovelRequest.Proprietario
+            //    };
+            //    imoveis.Add(imovel);
+            //    return Results.Ok(imovel);
+            //});
 
-            app.MapPut($"{url}/{{id}}", (string id, CreateImovel imovelAtualizado) =>
-            {
-                var imovel = imoveis.FirstOrDefault(i => i.Id == id);
-                if (imovel == null)
-                {
-                    return Results.NotFound();
-                }
-                if (imovelAtualizado == null || (string.IsNullOrEmpty(imovelAtualizado.Endereco) && string.IsNullOrEmpty(imovelAtualizado.Proprietario)))
-                {
-                    return Results.BadRequest();
-                }
-                if (!string.IsNullOrEmpty(imovelAtualizado.Endereco))
-                {
-                    imovel.Endereco = imovelAtualizado.Endereco;
-                }
-                if (!string.IsNullOrEmpty(imovelAtualizado.Proprietario))
-                {
-                    imovel.Proprietario = imovelAtualizado.Proprietario;
-                }
+            //app.MapPut($"{url}/{{id}}", (string id, CreateImovel imovelAtualizado) =>
+            //{
+            //    var imovel = imoveis.FirstOrDefault(i => i.Id == id);
+            //    if (imovel == null)
+            //    {
+            //        return Results.NotFound();
+            //    }
+            //    if (imovelAtualizado == null || (string.IsNullOrEmpty(imovelAtualizado.Endereco) && string.IsNullOrEmpty(imovelAtualizado.Proprietario)))
+            //    {
+            //        return Results.BadRequest();
+            //    }
+            //    if (!string.IsNullOrEmpty(imovelAtualizado.Endereco))
+            //    {
+            //        imovel.Endereco = imovelAtualizado.Endereco;
+            //    }
+            //    if (!string.IsNullOrEmpty(imovelAtualizado.Proprietario))
+            //    {
+            //        imovel.Proprietario = imovelAtualizado.Proprietario;
+            //    }
 
-                return Results.Ok(imovel);
-            });
+            //    return Results.Ok(imovel);
+            //});
 
-            app.MapDelete($"{url}/{{id}}", (string id) =>
-            {
-                var imovel = imoveis.FirstOrDefault(i => i.Id == id);
-                if (imovel == null)
-                {
-                    return Results.NotFound();
-                }
-                imoveis.Remove(imovel);
-                return Results.Ok(imovel);
-            });
-            app.MapGet($"{url}", () => "This is a GET");
+            //app.MapDelete($"{url}/{{id}}", (string id) =>
+            //{
+            //    var imovel = imoveis.FirstOrDefault(i => i.Id == id);
+            //    if (imovel == null)
+            //    {
+            //        return Results.NotFound();
+            //    }
+            //    imoveis.Remove(imovel);
+            //    return Results.Ok(imovel);
+            //});
+            //app.MapGet($"{url}", () => "This is a GET");
 
-            app.Run();
+            //app.Run();
 
 
 
 
             // Com Controllers:
 
-            //var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
-            //// Add services to the container.
+            // Add services to the container.
 
-            //builder.Services.AddControllers();
-            //// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            //builder.Services.AddEndpointsApiExplorer();
-            //builder.Services.AddSwaggerGen(
-            //    options =>
-            //    {
-            //        options.SwaggerDoc("v1", new OpenApiInfo
-            //        {
-            //            Version = "v1",
-            //            Title = "Imoveis API",
-            //            Description = "An API for managing real estate properties, allowing operations such as adding, retrieving, updating, and deleting properties",
-            //            TermsOfService = new Uri("https://example.com/terms"),
-            //            Contact = new OpenApiContact
-            //            {
-            //                Name = "Example Contact",
-            //                Url = new Uri("https://example.com/contact")
-            //            },
-            //            License = new OpenApiLicense
-            //            {
-            //                Name = "Example License",
-            //                Url = new Uri("https://example.com/license")
-            //            }
-            //        });
-            //        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            //        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            //    });
+            builder.Services.AddScoped<IContentTypeProvider, FileExtensionContentTypeProvider>();
 
-            //var app = builder.Build();
+            builder.Services.AddControllers();
 
-            //// Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Imoveis API",
+                        Description = "An API for managing real estate properties, allowing operations such as adding, retrieving, updating, and deleting properties",
+                        TermsOfService = new Uri("https://example.com/terms"),
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Example Contact",
+                            Url = new Uri("https://example.com/contact")
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Example License",
+                            Url = new Uri("https://example.com/license")
+                        }
+                    });
+                    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+                });
 
-            //app.UseHttpsRedirection();
+            var app = builder.Build();
 
-            //app.UseAuthorization();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
+            app.UseHttpsRedirection();
 
-            //app.MapControllers();
+            app.UseAuthorization();
 
-            //app.Run();
+            app.UseStaticFiles();
+
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
