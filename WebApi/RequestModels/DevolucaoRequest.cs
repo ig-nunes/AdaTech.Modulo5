2965a4ec41d2730532065354ddb797fa70de94e1
Igor Nunes <inunes.us@gmail.com>
2024-02-20 12:43:13 -0300

    feat: add request models

---
+++ WebApi/RequestModels/DevolucaoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.RequestModels
+{
+    public class DevolucaoRequest
+    {
+        public int VendaId { get; set; }
+        public int ItemId { get; set; }
+    }
+}
---
+++ WebApi/RequestModels/TrocaRequest.cs
@@ -0,0 +1,11 @@
+﻿using DadosSistema.Models;
+
+namespace WebApi.RequestModels
+{
+    public class TrocaRequest
+    {
+        public int VendaId { get; set; }
+        public int ItemVendaId { get; set; }
+        public Produto ProdutoNovo { get; set; }
+    }
+}
