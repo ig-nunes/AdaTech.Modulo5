8346eaaa4bf78a522f725160f9369373612f903e
Igor Nunes <inunes.us@gmail.com>
2024-02-20 12:40:39 -0300

    feat: Devolucao and Troca repositories in memory

---
+++ DadosSistema/DevolucaoRepositorioInMemory.cs
@@ -0,0 +1,36 @@
+﻿using DadosSistema.Models;
+using DadosSistema.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosSistema
+{
+    public class DevolucaoRepositorioInMemory : IRepositorio<Devolucao>
+    {
+        private List<Devolucao> _devolucoes = new List<Devolucao>()
+        {
+            new Devolucao(1, 1, DateTime.Now),
+        };
+
+        public Devolucao Add(Devolucao devolucao)
+        {
+            _devolucoes.Add(devolucao);
+            return devolucao;
+        }
+
+        public List<Devolucao> Get()
+        {
+            return _devolucoes;
+        }
+
+        public Devolucao? GetById(int id)
+        {
+            var devolucao = _devolucoes.FirstOrDefault(devolucao => devolucao.Id == id);
+
+            return devolucao;
+        }
+    }
+}
---
+++ DadosSistema/TrocaRepositorioInMemory.cs
@@ -0,0 +1,41 @@
+﻿using DadosSistema.Models;
+using DadosSistema.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosSistema
+{
+    public class TrocaRepositorioInMemory : IRepositorio<Troca>
+    {
+        private List<Troca> _trocas = new List<Troca>() 
+        {
+            new Troca(1, 1, new Produto("ProdutoNovo1", 10.0m)),
+            new Troca(2, 2, new Produto("ProdutoNovo2", 20.0m)),
+        };
+
+        public Troca Add(Troca troca)
+        {
+            _trocas.Add(troca);
+            return troca;
+        }
+
+        public List<Troca> Get()
+        {
+            return _trocas;
+        }
+
+        public Troca? GetById(int id)
+        {
+            return _trocas.FirstOrDefault(t => t.Id == id);
+        }
+
+        public List<Troca> GetTrocasPorVenda(int vendaId)
+        {
+            return _trocas.Where(t => t.VendaId == vendaId).ToList();
+        }
+    }
+}
