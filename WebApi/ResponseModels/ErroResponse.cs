3f61e457dbd669160331ff1676834bd580875ea2
Igor Nunes <inunes.us@gmail.com>
2024-02-20 12:45:19 -0300

    feat: create error response model

---
+++ WebApi/ResponseModels/ErroResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.ResponseModels
+{
+    public class ErroResponse
+    {
+        public string Titulo { get; set; }
+        public string Detalhes { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
