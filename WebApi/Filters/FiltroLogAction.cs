c9278945ac652e6ded225dac51f7390387d91fa7
Igor Nunes <inunes.us@gmail.com>
2024-02-21 11:30:21 -0300

    feat: Ilogger filter and middleware

---
+++ WebApi/Filters/FiltroLogAction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Filters
+{
+    public class FiltroLogAction : IActionFilter
+    {
+        private readonly ILogger _logger;
+
+        public FiltroLogAction(ILogger<FiltroLogAction> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("Final da ação");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("Início da ação");
+        }
+
+    }
+}
---
+++ WebApi/Middlewares/LogMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApi.Middlewares
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LogMiddleware(RequestDelegate next, ILogger<LogMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation("Início da ação - Middleware");
+
+            await _next(context);
+
+            _logger.LogInformation("Fim da ação - Middleware");
+        }
+    }
+}
